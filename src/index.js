f28ffbeea32d4240a75c5157f4409deaac87bf67
shahal abdulla <shahalabdulla04@gmail.com>
2026-03-13 00:16:47 +0530

    fix trust proxy

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const rateLimit = require('express-rate-limit')
 require('dotenv').config()
 
 const app = express()
+app.set('trust proxy', 1)
 
 // SECURITY — Helmet adds security headers
 app.use(helmet())
